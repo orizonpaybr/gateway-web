0a8964a4e7edbe4de408be0e34ab6b850e530448
Pedro Antonny <123685133+PedroAntonny@users.noreply.github.com>
2025-10-11 16:31:26 -0300

    feat: add icon/component documentation

---
+++ components/icons/DocumentIcon.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FileText } from 'lucide-react'
+
+interface DocumentIconProps {
+  size?: number
+  className?: string
+}
+
+export function DocumentIcon({ size = 28, className = '' }: DocumentIconProps) {
+  return (
+    <div
+      className={`flex items-center justify-center rounded-lg bg-blue-500 text-white ${className}`}
+      style={{ width: size, height: size }}
+    >
+      <FileText size={size * 0.7} />
+    </div>
+  )
+}
