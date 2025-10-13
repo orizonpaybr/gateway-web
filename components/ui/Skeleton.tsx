f0d6d2f16ea39612585d119a78a5f9eb149164c0
Pedro Antonny <123685133+PedroAntonny@users.noreply.github.com>
2025-10-13 17:09:22 -0300

    feat: add skeleton

---
+++ components/ui/Skeleton.tsx
@@ -0,0 +1,11 @@
+import { cn } from '@/lib/utils'
+
+interface SkeletonProps {
+  className?: string
+}
+
+export const Skeleton: React.FC<SkeletonProps> = ({ className }) => {
+  return (
+    <div className={cn('animate-pulse rounded-md bg-gray-200', className)} />
+  )
+}
