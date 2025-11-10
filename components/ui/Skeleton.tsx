196a5a336411b5b3b575de822dd9cabbccc93247
Pedro Antonny <123685133+PedroAntonny@users.noreply.github.com>
2025-11-10 15:41:41 -0300

    feat: add improved performance with React hooks.

--- components/ui/Skeleton.tsx
+++ components/ui/Skeleton.tsx
@@ -1,11 +1,12 @@
+import { memo } from 'react'
 import { cn } from '@/lib/utils'
 
 interface SkeletonProps {
   className?: string
 }
 
-export const Skeleton: React.FC<SkeletonProps> = ({ className }) => {
+export const Skeleton = memo<SkeletonProps>(function Skeleton({ className }) {
   return (
     <div className={cn('animate-pulse rounded-md bg-gray-200', className)} />
   )
-}
+})
